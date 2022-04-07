1c21e4d67ec277b6ba898e20eb6b9b29d6373e87
brinleymiller122 <miller.brinley@gmail.com>
2022-04-06 21:06:42 -0600

    add some onnx

--- intex2w/Models/Crash.cs
+++ intex2w/Models/Crash.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.ML.OnnxRuntime.Tensors;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -65,6 +66,16 @@ namespace intex2w.Models
         }
         [NotMapped]
         public bool WORK_ZONE_BOOL { get; set; }
+
+        public Tensor<float> AsTensor()
+        {
+            float[] data = new float[] 
+            {
+               
+            };
+            int[] dimensions = new int[] { 1, 8 };
+            return new DenseTensor<float>(data, dimensions);
+        }
 
     }
 }
