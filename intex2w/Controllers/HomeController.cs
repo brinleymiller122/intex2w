9b6070782c33e6e82fa38c5589a1b35e0eb5b0e1
brinleymiller122 <miller.brinley@gmail.com>
2022-04-07 14:09:37 -0600

    bad

--- intex2w/Controllers/HomeController.cs
+++ intex2w/Controllers/HomeController.cs
@@ -21,11 +21,11 @@ namespace intex2w.Controllers
         private readonly ILogger<HomeController> _logger;
         private DBContext _context;
 
-        public HomeController(ILogger<HomeController> logger, DBContext context)
+        public HomeController(ILogger<HomeController> logger, DBContext context, InferenceSession session)
         {
             _logger = logger;
             _context = context;
-            //_session = session;
+            _session = session;
         }
 
         [HttpGet]
@@ -427,7 +427,7 @@ namespace intex2w.Controllers
             {
                 NamedOnnxValue.CreateFromTensor("float_input", inputData.AsTensor())
             });
-            Tensor<string> score = result.First().AsTensor<string>();
+            Tensor<int> score = result.First().AsTensor<int>();
             var prediction = new Prediction { PredictedValue = score.ToString() };
             result.Dispose();
             return View("Score");
